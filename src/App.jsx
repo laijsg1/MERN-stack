ac320beb32a6329cd886a63a23d36c2e64fe84a5
laisg <laijsg@163.com>
2019-03-18 21:06:40 +0800

    组件1

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,43 @@
 const contentNode = document.getElementById('contents');
 
-const continents=['af','am','as'];
-const message=continents.map(c=> `Hello ${c}!`).join(' ');
+class IssueFilter extends React.Component{
+    render() {
+        return (
+            <div>This is a placeholder for the issue filter.</div>
+        );
+    }
+}
+class IssueTable extends React.Component{
+    render() {
+        return (
+            <div>This is a placeholder for a table of Issues .</div>
+        );
+    }
+}
+class IssueAdd extends React.Component{
+    render() {
+        return (
+            <div>This is a placeholder for an issue Add entry form .</div>
+        );
+    }
+}
+class IssueList extends React.Component{
+    render() {
+        return (
+            <div>
+            <h1>Issues Tracker</h1>
+            <IssueFilter/>
+            <hr />
+            
+            <IssueTable/>
+            <hr />
+            
+            <IssueAdd/>
+            
+            </div>
+        );
+    }
+}
 
-const component = <h1>{message}</h1>;        // A simple component, written in JSX
 
-ReactDOM.render(component, contentNode);      // Render the component inside the content Node
+ReactDOM.render(<IssueList />, contentNode);      // Render the component inside the content Node
