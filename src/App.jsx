d67a4e979332c2b2d87ff7454ac27b387d8299a3
laisg <laijsg@163.com>
2019-03-20 23:58:04 +0800

    Dynamic Composition

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,45 @@ class IssueFilter extends React.Component{
         );
     }
 }
+class IssueRow extends React.Component{
+    render() {
+        const issue=this.props.issue;
+        return (
+            <tr>
+                <td>{issue.id}</td>
+                <td>{issue.status}</td>
+                <td>{issue.owner}</td>
+                <td>{issue.created.toDateString()}</td>
+                <td>{issue.effort}</td>
+                <td>{issue.completionDate ?issue.completionDate.toDateString() : ''}</td>
+                <td>{issue.title}</td>
+            </tr >
+
+        );
+    }
+}
 class IssueTable extends React.Component{
     render() {
+        //const borderedStyle={border: "1px solid silver",padding: 4};
+        const issueRows=this.props.issues.map(issue => <IssueRow key={issue.id} issue={issue} />)
         return (
-            <div>This is a placeholder for a table of Issues .</div>
+            <table className="bordered-table"> 
+                <thead>
+                    <tr>
+                        <th >Id</th>
+                        <th >Status</th>
+                        <th >Owner</th>
+                        <th >Created</th>
+                        <th >Effort</th>
+                        <th >Completion</th>
+                        <th >Title</th>
+                    </tr>
+                </thead> 
+                <tbody>
+
+                    {issueRows}
+                </tbody>
+            </table>
         );
     }
 }
@@ -21,6 +56,20 @@ class IssueAdd extends React.Component{
         );
     }
 }
+
+const issues = [
+  {
+    id: 1, status: 'Open', owner: 'Ravan',
+    created: new Date('2016-08-15'), effort: 5, completionDate: undefined,
+    title: 'Error in console when clicking Add',
+  },
+  {
+    id: 2, status: 'Assigned', owner: 'Eddie',
+    created: new Date('2016-08-16'), effort: 14, completionDate: new Date('2016-08-30'),
+    title: 'Missing bottom border on panel',
+  },
+];
+
 class IssueList extends React.Component{
     render() {
         return (
@@ -29,7 +78,7 @@ class IssueList extends React.Component{
             <IssueFilter/>
             <hr />
             
-            <IssueTable/>
+            <IssueTable issues={issues}/>
             <hr />
             
             <IssueAdd/>
